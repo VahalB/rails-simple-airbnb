07f7b982ebf4c89251a34189d2cbc4ccee9334a6
Valentin Beau <valentinbeau.fr@gmail.com>
2023-03-29 11:15:52 +0200

    rails new

---
+++ db/migrate/20230222141857_create_flags.rb
@@ -0,0 +1,13 @@
+class CreateFlags < ActiveRecord::Migration[7.0]
+  def change
+    create_table :flags do |t|
+      t.string :name
+      t.string :address
+      t.text :description
+      t.integer :price_per_night
+      t.integer :number_of_guests
+
+      t.timestamps
+    end
+  end
+end
